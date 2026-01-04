5378afaa8d8b79c19f6c1d9581d6f54c2f3e07a5
Jacob Doucet <jacobdoucet@MacBook-Pro-2.local>
2026-01-04 12:32:48 +0000

    fix versioning

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -13,11 +14,23 @@ var (
 	BuildDate = "unknown"
 )
 
+func getVersion() string {
+	// If version was set via ldflags, use it
+	if Version != "dev" {
+		return Version
+	}
+	// Otherwise try to get it from build info (works with go install)
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	return Version
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("forge %s\n", Version)
+		fmt.Printf("forge %s\n", getVersion())
 		if Commit != "none" {
 			fmt.Printf("  commit: %s\n", Commit)
 		}
